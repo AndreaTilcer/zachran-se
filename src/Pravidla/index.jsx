f89106d3a4801eac35e4b2e26db7495f59612683
Andrea Tilcerová <andrea.tilcer@gmail.com>
2022-06-02 13:44:04 +0200

    Přidání Pravidla navigace

--- src/Pravidla/index.jsx
+++ src/Pravidla/index.jsx
@@ -1,3 +1,41 @@
 import React from 'react';
+import '../style.css';
+import './style.css';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import Play from '../img/play.svg';
+import Question from '../img/question.svg';
+import Home from '../img/home.svg';
 
-export const Pravidla = () => {};
+export const Pravidla = () => {
+  return (
+    <>
+      <div className="rules_container">
+        <nav className="rules_navigation">
+          <Link to="/hra">
+            <img className="homepage_button" src={Play} alt="hrat-znovu" />
+          </Link>{' '}
+          <Link to="/">
+            <img className="homepage_button" src={Home} alt="domu" />
+          </Link>{' '}
+          <Link to="/pravidla">
+            <img className="homepage_button" src={Question} alt="pravidla" />{' '}
+          </Link>
+        </nav>
+        <Outlet />
+
+        <div>
+          <h1 className="rules_heading">O hře Zachraň se</h1>
+
+          <a href="http://andreatilcer.github.io/o-projektu/">web</a>
+        </div>
+        <div>
+          <h1 className="rules_heading">Jak hrát</h1>
+        </div>
+
+        <div>
+          <h1 className="rules_heading">Co je cílem hry</h1>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/index-a.jsx
+++ src/index-a.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
-import { Hra } from './Hra';
+import { ZacitHrat } from './ZacitHrat';
 import { Pravidla } from './Pravidla';
 import Play from './img/play.svg';
 import Question from './img/question.svg';
@@ -15,28 +14,21 @@ const App = () => (
 
     <img className="homepage_monkey" src={Monkey} alt="opice" />
     <img className="homepage_call" src={EmergencyCall} alt="tisnove-volani" />
+
     <nav className="homepage_navigation">
-      <Link to="/hra">
+      <a href="/ZacitHrat">
         <img
           className="homepage_button homepage_button1"
           src={Play}
           alt="zacit-hrat"
         />
-      </Link>{' '}
-      <Link to="/pravidla">
-        <img className="homepage_button" src={Question} alt="pravidla" />{' '}
-      </Link>
+      </a>{' '}
+      {''}
+      <a href="/Pravidla">
+        <img className="homepage_button" src={Question} alt="pravidla" />
+      </a>
     </nav>
-    <Outlet />
   </div>
 );
 
-createRoot(document.querySelector('#app')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="hra" element={<Hra />} />
-      <Route path="pravidla" element={<Pravidla />} />
-    </Routes>
-  </BrowserRouter>,
-);
+createRoot(document.querySelector('#app')).render(<App />);
