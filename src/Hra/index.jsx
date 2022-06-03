bc48828ab505428498432f5e8601087b16aa8e1a
Milota Styková <you@example.com>
2022-06-03 17:22:33 +0200

    opička a posúvanie opičky

--- src/Hra/index.jsx
+++ src/Hra/index.jsx
@@ -46,11 +46,11 @@ export const Hra = () => {
         </nav>
         <div className="lifebar">
           <img className="lifebar_monkey" src={Monkey} alt="Opička" />
-          <img className="lifebar_hearth" src={Heart} alt="Život" />
-          <img className="lifebar_hearth" src={Heart} alt="Život" />
-          <img className="lifebar_hearth" src={Heart} alt="Život" />
-          <img className="lifebar_hearth" src={Heart} alt="Život" />
-          <img className="lifebar_hearth" src={Heart} alt="Život" />
+          <img className="lifebar_heart" src={Heart} alt="Život" />
+          <img className="lifebar_heart" src={Heart} alt="Život" />
+          <img className="lifebar_heart" src={Heart} alt="Život" />
+          <img className="lifebar_heart" src={Heart} alt="Život" />
+          <img className="lifebar_heart" src={Heart} alt="Život" />
         </div>
       </div>
       <Outlet />
@@ -68,36 +68,55 @@ export const Hra = () => {
           <div className="tree tree4">
             <img src={Tree4} alt="Strom" />
           </div>
+          <div className="path">
+            <img src={Path} alt="Cesta" />
+          </div>
           <div className="house">
-            <img src={House} alt="Chata" onClick={Chata} />
+            <img src={House} alt="Chata" onClick={() => {
+              document.querySelector("#player").classList.add("monkey1")
+            }} />
           </div>
           <div className="storm">
-            <img src={Storm} alt="Bouřka" onClick={Bourka} />
+            <img src={Storm} alt="Bouřka" onClick={() => {
+              document.querySelector("#player").classList.add("monkey2")
+            }} />
           </div>
           <div className="electricity">
             <img
               src={Electricity}
               alt="Dráty vysokého napětí"
-              onClick={Draty}
+              onClick={() => {
+                document.querySelector("#player").classList.add("monkey3")
+              }}
             />
           </div>
           <div className="accident">
-            <img src={Accident} alt="Autonehoda" onClick={Autonehoda} />
+            <img src={Accident} alt="Autonehoda" onClick={() => {
+              document.querySelector("#player").classList.add("monkey4")
+            }} />
           </div>
           <div className="river">
-            <img src={River} alt="Řeka" onClick={Reka} />
+            <img src={River} alt="Řeka" onClick={() => {
+              document.querySelector("#player").classList.add("monkey5")
+            }} />
           </div>
           <div className="fireplace">
-            <img src={Fireplace} alt="Ohniště" onClick={Ohniste} />
+            <img src={Fireplace} alt="Ohniště" onClick={() => {
+              document.querySelector("#player").classList.add("monkey6")
+            }} />
           </div>
           <div className="burnt">
-            <img src={Burnt} alt="Hoří oděv" onClick={Popaleny} />
+            <img src={Burnt} alt="Hoří oděv" onClick={() => {
+              document.querySelector("#player").classList.add("monkey7")
+            }} />
           </div>
           <div className="snow">
-            <img src={Snow} alt="Lavina" onClick={Lavina} />
+            <img src={Snow} alt="Lavina" onClick={() => {
+              document.querySelector("#player").classList.add("monkey8")
+            }} />
           </div>
-          <div className="path">
-            <img src={Path} alt="Cesta" />
+          <div className="monkey" id='player'>
+            <img src={Monkey} alt="Hráč" />
           </div>
         </div>
       </div>
