29bd6a6029f14ca952631f80bc78e455542abb5b
Andrea Tilcerová <andrea.tilcer@gmail.com>
2022-06-03 11:23:39 +0200

    Pozicování gamefield

--- src/Hra/index.jsx
+++ src/Hra/index.jsx
@@ -35,69 +35,69 @@ export const Hra = () => {
       <div className="game_container">
         <nav className="game_navigation">
           <Link to="/hra">
-            <img className="homepage_button" src={Refresh} alt="hrat-znovu" />
+            <img className="game_button" src={Refresh} alt="hrat-znovu" />
           </Link>{' '}
           <Link to="/">
-            <img className="homepage_button" src={Home} alt="domu" />
+            <img className="game_button" src={Home} alt="domu" />
           </Link>{' '}
           <Link to="/pravidla">
-            <img className="homepage_button" src={Question} alt="pravidla" />{' '}
+            <img className="game_button" src={Question} alt="pravidla" />{' '}
           </Link>
         </nav>
         <div className="lifebar">
-          <img src={Monkey} alt="Opička" />
-          <img src={Heart} alt="Život" />
-          <img src={Heart} alt="Život" />
-          <img src={Heart} alt="Život" />
-          <img src={Heart} alt="Život" />
-          <img src={Heart} alt="Život" />
+          <img className="lifebar_monkey" src={Monkey} alt="Opička" />
+          <img className="lifebar_hearth" src={Heart} alt="Život" />
+          <img className="lifebar_hearth" src={Heart} alt="Život" />
+          <img className="lifebar_hearth" src={Heart} alt="Život" />
+          <img className="lifebar_hearth" src={Heart} alt="Život" />
+          <img className="lifebar_hearth" src={Heart} alt="Život" />
         </div>
-        <Outlet />
-        <div className="game">
-          <div className="gamefield">
-            <div className="tree tree1">
-              <img src={Tree1} alt="Strom" />
-            </div>
-            <div className="tree tree2">
-              <img src={Tree2} alt="Strom" />
-            </div>
-            <div className="tree tree3">
-              <img src={Tree3} alt="Strom" />
-            </div>
-            <div className="tree tree4">
-              <img src={Tree4} alt="Strom" />
-            </div>
-            <div className="house">
-              <img src={House} alt="Chata" onClick={Chata} />
-            </div>
-            <div className="storm">
-              <img src={Storm} alt="Bouřka" onClick={Bourka} />
-            </div>
-            <div className="electricity">
-              <img
-                src={Electricity}
-                alt="Dráty vysokého napětí"
-                onClick={Draty}
-              />
-            </div>
-            <div className="accident">
-              <img src={Accident} alt="Autonehoda" onClick={Autonehoda} />
-            </div>
-            <div className="river">
-              <img src={River} alt="Řeka" onClick={Reka} />
-            </div>
-            <div className="fireplace">
-              <img src={Fireplace} alt="Ohniště" onClick={Ohniste} />
-            </div>
-            <div className="burnt">
-              <img src={Burnt} alt="Hoří oděv" onClick={Popaleny} />
-            </div>
-            <div className="snow">
-              <img src={Snow} alt="Lavina" onClick={Lavina} />
-            </div>
-            <div className="path">
-              <img src={Path} alt="Cesta" />
-            </div>
+      </div>
+      <Outlet />
+      <div className="game">
+        <div className="gamefield">
+          <div className="tree tree1">
+            <img src={Tree1} alt="Strom" />
+          </div>
+          <div className="tree tree2">
+            <img src={Tree2} alt="Strom" />
+          </div>
+          <div className="tree tree3">
+            <img src={Tree3} alt="Strom" />
+          </div>
+          <div className="tree tree4">
+            <img src={Tree4} alt="Strom" />
+          </div>
+          <div className="house">
+            <img src={House} alt="Chata" onClick={Chata} />
+          </div>
+          <div className="storm">
+            <img src={Storm} alt="Bouřka" onClick={Bourka} />
+          </div>
+          <div className="electricity">
+            <img
+              src={Electricity}
+              alt="Dráty vysokého napětí"
+              onClick={Draty}
+            />
+          </div>
+          <div className="accident">
+            <img src={Accident} alt="Autonehoda" onClick={Autonehoda} />
+          </div>
+          <div className="river">
+            <img src={River} alt="Řeka" onClick={Reka} />
+          </div>
+          <div className="fireplace">
+            <img src={Fireplace} alt="Ohniště" onClick={Ohniste} />
+          </div>
+          <div className="burnt">
+            <img src={Burnt} alt="Hoří oděv" onClick={Popaleny} />
+          </div>
+          <div className="snow">
+            <img src={Snow} alt="Lavina" onClick={Lavina} />
+          </div>
+          <div className="path">
+            <img src={Path} alt="Cesta" />
           </div>
         </div>
       </div>
